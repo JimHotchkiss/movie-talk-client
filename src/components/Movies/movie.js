25c562956e7160bb4c9b011c63db6de2be10774a
github username <github email address>
2019-12-26 14:09:05 -0800

    Added styling with Bootstrap

--- src/components/Movies/movie.js
+++ src/components/Movies/movie.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
 
 const Movie = props => {
   return (
-    <div>
-      <Card style={{ width: "185px" }}>
+    <Container>
+      <Card style={{ width: "185px", margin: "1em" }}>
         <Card.Img
           height="278px"
           variant="top"
@@ -12,12 +13,18 @@ const Movie = props => {
             "https://image.tmdb.org/t/p/w185_and_h278_bestv2" + props.posterUrl
           }
         />
-        <Card.Body>
+        <Card.Body
+          style={{
+            overflow: "hidden",
+            overflowY: "scroll",
+            height: "278px"
+          }}
+        >
           <Card.Title>{props.title}</Card.Title>
           <Card.Text>{props.description}</Card.Text>
         </Card.Body>
       </Card>
-    </div>
+    </Container>
   );
 };
 export default Movie;
