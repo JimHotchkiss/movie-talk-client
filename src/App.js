2335c140cdfe87b52ec52d059110ad11f6d53b40
github username <github email address>
2019-12-26 08:56:57 -0800

    Added react-bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.css";
 import Movies from "./components/Movies/movies";
 
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
+
 function App() {
   return (
     <div className="App">
